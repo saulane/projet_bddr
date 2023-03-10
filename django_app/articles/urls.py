3e8debda71c3f195ea604f9e37b1f1c75a2b738a
Saulane <rave.simon@live.fr>
2023-03-10 23:47:11 +0100

    recherche par sous-theme

--- django_app/articles/urls.py
+++ django_app/articles/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
     path("articles/", views.articles, name="articles"),
+    path("articles/<path:doi>/", views.article),
     path("", views.home, name="home")
 ]
--- django_app/articles/views.py
+++ django_app/articles/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Article
+from .models import Article,Subtheme,Theme
 
 def articles(request):
-    s = request.GET.get('title')
-    print(s)
-    context = {"res": Article.objects.filter(title__icontains=s)}
+    title = request.GET.get('title')
+    subtheme = request.GET.get('subtheme')
+    q = Article.objects
+    if title:
+        q = q.filter(title__icontains=title)
+
+    if subtheme:
+        q = q.filter(subtheme_id=subtheme)
+    context = {"res": q}
 
     template = loader.get_template('articles.html')
     return HttpResponse(template.render(context))
 
+def article(request, doi):
+    template = loader.get_template('article.html')
+    context = {"res": Article.objects.get(doi=doi)}
+    return HttpResponse(template.render(context))
+
 def home(request):
+    context = {"subthemes": Subtheme.objects.all()}
+
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context))
